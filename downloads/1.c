cf0bdc71efb1a4bc7101555adfd4cc1f17ab10a2
Lin41223107 <144769822+Lin41223107@users.noreply.github.com>
2023-12-31 08:39:35 +0000

    新增ANSIC頁面

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 黃詠琳\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : september 7, 2004\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0971-797307\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
